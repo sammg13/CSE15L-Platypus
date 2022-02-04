a0d0565221016fea2326b15b28305701b32ccf39
sammg13 <s3gonzal@ucsd.edu>
2022-02-03 18:44:59 -0800

    corrected test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 2 + 1);
+        assertEquals(2, 1 + 1);
     }
 
     @Test
