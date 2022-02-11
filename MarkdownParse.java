ca5e9233accdad3ab11e710fb638a2d48c0448a5
sammg13 <s3gonzal@ucsd.edu>
2022-02-10 18:21:28 -0800

    commented out test 6

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -39,4 +39,5 @@ public class MarkdownParse {
         System.out.println(links);
     }
 }
-// remove comment
+// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+// java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
