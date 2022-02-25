6ef511038897d3000b61bba9125a67e90c34d3df
sammg13 <s3gonzal@ucsd.edu>
2022-02-25 10:27:13 -0800

    added more test files and corresponding tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -85,4 +85,37 @@ public class MarkdownParseTest {
         ArrayList<String> expected = new ArrayList<String>();
         assertEquals(expected,links);
     }
+    @Test
+    public void testSnippet1() throws IOException {
+        Path fileName = Path.of("snippet-1.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("`google.com");
+        expected.add("google.com");
+        expected.add("ucsd.edu");
+        assertEquals(expected,links);
+    }
+    @Test
+    public void testSnippet2() throws IOException {
+        Path fileName = Path.of("snippet-2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("a.com");
+        expected.add("a.com(())");
+        expected.add("example.com");
+        assertEquals(expected,links);
+    }
+    @Test
+    public void testSnippet3() throws IOException {
+        Path fileName = Path.of("snippet-3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("https://www.twitter.com");
+        expected.add("https://ucsd-cse15l-w22.github.io/");
+        expected.add("https://cse.ucsd.edu/");
+        assertEquals(expected,links);
+    }
 }
